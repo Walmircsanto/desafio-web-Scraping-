c8997893fd2a6f500d69f917ad66288249817218
Walmircsanto <walmirsantosrosa@gmail.com>
2025-03-28 15:00:16 -0300

    :recycle: refactor: refatorado metodo de buscar todos os pdf pela URL

---
+++ src/main/java/org/example/scraping/DownloadPDF.java
@@ -0,0 +1,4 @@
+package org.example.scraping;
+
+public class DownloadPDF {
+}
---
+++ src/main/java/org/example/scraping/GetAllPdfsByURL.java
@@ -0,0 +1,56 @@
+package org.example.scraping;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipOutputStream;
+
+import static org.example.scraping.ZipparPdf.compactarPDFs;
+
+public class GetAllPdfsByURL {
+
+
+        public void getLinkPDF(String urlLink) throws IOException {
+            String pdfsDIR = "src/main/pdfs";
+            String zipFilePath = "src/main/pdfs/pdfs-gov.zip";
+
+            Files.createDirectories(Paths.get(pdfsDIR));
+            Files.createDirectories(Paths.get(zipFilePath).getParent());
+
+            Document doc = Jsoup.connect(urlLink).get();
+            Elements linksPdfs = doc.select("li a[href$=.pdf]");
+
+            List<String> pdfPaths = new ArrayList<>();
+
+            for (Element linkPdf : linksPdfs) {
+                String link = linkPdf.attr("abs:href");
+                String fileName = link.substring(link.lastIndexOf("/") + 1);
+
+                try {
+                    if (fileName.split("_")[0].equalsIgnoreCase("Anexo")) {
+                        String filePath = pdfsDIR + File.separator + fileName;
+
+                        DownloadPDF.dowloadPdf(link, filePath);
+                        pdfPaths.add(filePath); // so e adicionado assim que o pdf ja estiver baixado
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Compactar todos os PDFs em um único arquivo ZIP
+            compactarPDFs(pdfPaths, zipFilePath);
+        }
+
+
+}
+
+
+
