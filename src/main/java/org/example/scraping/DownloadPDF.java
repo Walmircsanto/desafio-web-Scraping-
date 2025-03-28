6d5cff3abb52ca5b917d48dd3807f492ec1e08be
Walmircsanto <walmirsantosrosa@gmail.com>
2025-03-28 15:03:34 -0300

    :sparkles: feat: Cria metodos de baixar pdf via URL

--- src/main/java/org/example/scraping/DownloadPDF.java
+++ src/main/java/org/example/scraping/DownloadPDF.java
@@ -1,4 +1,20 @@
 package org.example.scraping;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 public class DownloadPDF {
+
+    public  static void dowloadPdf(String url,  String file) throws IOException {
+        //indico que a URL chegando  e um recurso
+        URL urlPdf = new URL(url);            //inicia uma nova conexão TCP com o servidor para o qual a URL resolve
+        ReadableByteChannel canal = Channels.newChannel(urlPdf.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(canal,0, Long.MAX_VALUE); // abro um canal para receber esses bytes
+        fos.close();
+        canal.close();
+    }
 }
