61f38fca03e1cb82e500b805d8847996f6e34f77
Walmircsanto <walmirsantosrosa@gmail.com>
2025-03-29 23:08:53 -0300

    :zap: perf: Altera o modo com que era compactado o pdf e agora o pdf e diretamente compactado sem baixar

--- src/main/java/org/example/scraping/ZipparPdf.java
+++ src/main/java/org/example/scraping/ZipparPdf.java
@@ -1,6 +1,9 @@
 package org.example.scraping;
 
 import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -9,11 +12,11 @@ import java.util.zip.ZipOutputStream;
 public class ZipparPdf {
     static final int TAMANHO_BUFFER = 4096;
 
-    public static void compactarPDFs(List<String> pdfPaths, String zipFilePath) {
+    public static void compactarPDFs(String url,List<String> pdfPaths, String zipFilePath) {
                                                         //criara um canal de fluxo de saida de bytes
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFilePath))) {
             for (String pdfPath : pdfPaths) {
-                adicionarAoZip(pdfPath, zos);
+               adicionarAoZip(url, pdfPath, zos);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -21,25 +24,19 @@ public class ZipparPdf {
     }
 
 
-    private static void adicionarAoZip(String pdfPath, ZipOutputStream zos) throws IOException {
-                                                               //Abre um fluxo de entrada para ler bytes do arquivo
-        try (BufferedInputStream buf = new BufferedInputStream(new FileInputStream(pdfPath))) { //abrindo o pdf para poder ler o bytes
-            File file = new File(pdfPath);
+    private static void adicionarAoZip( String pdfUrl ,String pdfName, ZipOutputStream zos) throws IOException {
+
+        URL urlPdf = new URL(pdfUrl); //inicia uma nova conexão TCP com o servidor para o qual a URL resolve                                          //Abre um fluxo de entrada para ler bytes do arquivo
+        try (BufferedInputStream buf = new BufferedInputStream(urlPdf.openStream())){ //abrindo o pdf para poder ler o bytes
+
+            File file = new File(pdfName);
             ZipEntry zipEntry = new ZipEntry(file.getName());
             zos.putNextEntry(zipEntry);
 
-            byte[] buffer = new byte[4096];
+            byte[] buffer = new byte[TAMANHO_BUFFER];
             int bytesRead;
             while ((bytesRead = buf.read(buffer)) != -1) {
                 zos.write(buffer, 0, bytesRead);
-            }
-
-
-            if(file.exists()){
-                file.delete();
-                System.out.println("Arquivo apagado");
-            }else{
-                System.out.println("Não apagado");
             }
             zos.closeEntry();
 
